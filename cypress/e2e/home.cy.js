4743d762eb6cc1c79421916db441e7d52f28dbc5
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-18 14:59:58 -0700

    feat: test homepage for products

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,14 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.contains("Welcome to The Jungle");
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
