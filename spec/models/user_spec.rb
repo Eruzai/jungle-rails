a18ab299958ea781af44139659d1f16954e9bb74
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-18 09:40:30 -0700

    feat: test password length is 8 or more chars

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given password confirmation and password that do not match' do
-      it 'does not save the user to the database' do
+      it 'returns the error "Password confirmation doesn\'t match Password"' do
         @user = User.create( first_name: 'Johnny', last_name: 'Test', email: 'jtest@email.com', password: 'test1234', password_confirmation: 'test123')
 
         expect(@user).not_to be_valid
@@ -23,7 +23,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given no password' do
-      it 'does not save the user to the database' do
+      it 'returns the error "Password can\'t be blank"' do
         @user = User.create( first_name: 'Johnny', last_name: 'Test', email: 'jtest@email.com', password: nil, password_confirmation: nil)
 
         expect(@user).not_to be_valid
@@ -32,7 +32,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given no email' do
-      it 'does not save the user to the database' do
+      it 'returns the error "Email can\'t be blank"' do
         @user = User.create( first_name: 'Johnny', last_name: 'Test', email: nil, password: 'test1234', password_confirmation: 'test1234')
 
         expect(@user).not_to be_valid
@@ -41,7 +41,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given no first name' do
-      it 'does not save the user to the database' do
+      it 'returns the error "First name can\'t be blank"' do
         @user = User.create( first_name: nil, last_name: 'Test', email: 'jtest@email.com', password: 'test1234', password_confirmation: 'test1234')
 
         expect(@user).not_to be_valid
@@ -50,7 +50,7 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given no last name' do
-      it 'does not save the user to the database' do
+      it 'returns the error "Last name can\'t be blank"' do
         @user = User.create( first_name: 'Johnny', last_name: nil, email: 'jtest@email.com', password: 'test1234', password_confirmation: 'test1234')
 
         expect(@user).not_to be_valid
@@ -59,13 +59,22 @@ RSpec.describe User, type: :model do
     end
 
     describe 'given an email that exists in the database regardless of case' do
-      it 'does not save the user to the database' do
+      it 'returns the error "Email has already been taken"' do
         @user = User.create( first_name: 'Johnny', last_name: 'Test', email: 'jtest@email.com', password: 'test1234', password_confirmation: 'test1234')
         @user2 = User.create( first_name: 'Jack', last_name: 'Test', email: 'JTest@email.com', password: 'test1234', password_confirmation: 'test1234')
 
         expect(@user).to be_valid
         expect(@user2).not_to be_valid
         expect(@user2.errors.full_messages).to include("Email has already been taken")
+      end
+    end
+
+    describe 'given a password that is too short' do
+      it 'returns the error "Password is too short (minimum is 8 characters)"' do
+        @user = User.create( first_name: 'Johnny', last_name: 'Test', email: 'jtest@email.com', password: 'test123', password_confirmation: 'test123')
+
+        expect(@user).not_to be_valid
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
       end
     end
 
