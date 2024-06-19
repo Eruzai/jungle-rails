8d7c4a0f56542a55c77e14426f9b01737275470e
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-19 14:13:59 -0700

    feat: define message with order parameters

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,6 @@
 class OrderMailer < ApplicationMailer
+  def order_receipt_email
+    @order = params[:order]
+    mail(to: @order.email, subject: "Your Order no. #{@order.id} From Jungle")
+  end
 end
