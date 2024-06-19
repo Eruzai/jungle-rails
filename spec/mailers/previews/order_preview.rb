64caacb7c74d23cb011cd0f94a94194c293a96d2
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-19 14:14:41 -0700

    feat: create preview url for email templates

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
 class OrderPreview < ActionMailer::Preview
-
+  def order_receipt_email
+    OrderMailer.with(order: Order.last).order_receipt_email
+  end
 end
