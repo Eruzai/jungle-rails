78bb2161fa95da9863b6bd4a0d77f077f1fac768
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-21 09:17:38 -0700

    feat: implement after_create callback to deduct quantities from stock after order creation

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ApplicationRecord
 
   validates :stripe_charge_id, presence: true
 
+  after_create :deduct_order_quantities_from_stock
+
+  private
+
+  def deduct_order_quantities_from_stock
+    self.line_items.each do |item|
+      @product = Product.find(item.product.id)
+      @product.quantity = @product.quantity - item.quantity
+      @product.save
+    end
+  end
+
 end
