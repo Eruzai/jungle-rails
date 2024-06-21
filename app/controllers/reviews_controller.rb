14a6ce913ac0a20445767dab874e79d4474bfe45
Kevin Levasseur <kevin_levasseur@telus.net>
2024-06-20 17:35:16 -0700

    feat: implement posting reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,24 @@
 class ReviewsController < ApplicationController
   def create
-    raise "yay, I'm here!"
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to "/products/#{params[:product_id]}", notice: 'Review created!'
+    else
+      render "products/show", params: [@reviews = @product.reviews.includes(:user)]
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :product_id,
+      :user_id,
+      :description,
+      :rating
+    )
   end
 end
